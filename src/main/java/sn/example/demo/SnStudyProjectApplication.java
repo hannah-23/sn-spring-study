90605b0d93d5d59223c5fcc921b0a96fb1e8d84d
amjay <amjay.31@gmail.com>
2021-04-20 18:50:28 +0900

    first commit

--- src/main/java/sn/example/demo/SnStudyProjectApplication.java
+++ src/main/java/sn/example/demo/SnStudyProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SnStudyProjectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SnStudyProjectApplication.class, args);
 	}
 
